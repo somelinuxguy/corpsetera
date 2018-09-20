6a1b6a2058a4e1e50d537856efa9c63dc52d37e5
SomeLinuxGuy <github@sect.net>
2018-09-20 15:54:29 -0400

    housekeeping

--- src/navbar.js
+++ src/navbar.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 
+// let's add a count of cart items some day:
+// props.cart.length should give me a number.
+// we should pass cart to the NavBar connector too!
+
 let NavBar = (props) =>
     <div>
         <a href="/">All Things</a> 
@@ -13,6 +17,7 @@ let NavBar = (props) =>
         )}
     </div>
 
-let connectedNavBar = connect( (state) => ({categories : state.categories}) )(NavBar)
-
-export default connectedNavBar;
+//let ConnectedNavBar = connect( (state) => ({categories : state.categories}) )(NavBar)
+//export default ConnectedNavBar;
+// or this way, which is shorter:
+export default connect( (state) => ({categories : state.categories}) )(NavBar);
