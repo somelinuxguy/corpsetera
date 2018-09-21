6ff06dc2bf772c93a6f99dfa392e13ba55eea26b
SomeLinuxGuy <github@sect.net>
2018-09-21 09:53:32 -0400

    let us have a homepage for great justice

--- src/homepage.js
+++ src/homepage.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
-let HomePage = (props) =>
-    <div>
-        <p>This is the home page. I will list all items here.</p>
-    </div>
+let HomePage = (props) => {
+    return props.products ? 
+        <div className="productList" >
+            <h1>ALL THE THINGS</h1>
+            {props.products.map(product => 
+            <div className="product" key={product.id}>
+                <p> <Link to={`/products/${product.id}`}>{product.title}</Link> </p>
+                <img src={product.imageURL} alt={product.title} height="25" width="25"/>
+            </div>
+            )}
+        </div> 
+        :
+        <p>Categories Not Found</p>
+}
 
-export default HomePage;
+
+let ConnectedHomePage = connect( (state) => ({products: state.products}) )(HomePage)
+
+export default ConnectedHomePage;
